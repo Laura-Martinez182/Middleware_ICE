5deece8149b41de47e6a2f03f96de274bdde87bd
Laura-Martinez182 <71099708+Laura-Martinez182@users.noreply.github.com>
2023-03-06 18:21:00 -0500

    Second commit

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -1,4 +1,8 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.UUID;
 
 public class Client
 {
@@ -11,42 +15,29 @@ public class Client
             if(printer == null)
             {
                 throw new Error("Invalid proxy");
-            }
-            
-            // printer.printString("Arguments:");
-            //     for(String i : args)
-            //     {
-            //        printer.printString(i);
-            //     } 
-            
+            }    
              
-            //boolean response = verifyNumber(args[0]);
-            
 
-        //    if (args.length == 0) {
-        //     printer.printString("Debe ingresar al menos un entero positivo y opcionalmente un nombre de archivo de texto local \n" + "Autoras: \n" + "Keren Lopez - A00368902 \n" +
-        //     "Laura Martinez - A00365187");
-        //    } else if (args.length == 1) {
-        //     boolean response = verifyNumber(args[0]);
-        //     if (response == false) {
-        //         printer.printString("Debe ingresar un numero entero positivo mayor que 1");    
-        //     }
-        //    }
-
-            
-            // if(response == false){
-            //     printer.printString("The number is not valid");
-            // }
-            
-            //fileExists();
-            boolean exist = fileExists(args[1]);
-
-            if(exist == true) {
-                printer.printString("File exists!!");
-            } else {
-                printer.printString("File doesn't exist or program doesn't have access to the file");
-            }
-
+           if (args.length == 0) {
+            printer.printString("Debe ingresar al menos un entero positivo y opcionalmente un nombre de archivo de texto local \n" + "Autoras: \n" + "Keren Lopez - A00368902 \n" +
+            "Laura Martinez - A00365187");
+           } else{
+                boolean response = verifyNumber(args[0]);
+                if (response == false) {
+                    printer.printString("Debe ingresar un numero entero positivo mayor que 1");    
+                } else if(args.length == 1) {
+                    printer.printString(generateRandomGUII());
+                }else{
+                    boolean exist = fileExists(args[1]);
+                    if(exist == true) {
+                        printer.printString("File exists!!");
+                        String uuid = readUUID(args[1]);
+                        printer.printString(uuid);
+                    } else {
+                        printer.printString("File doesn't exist or program doesn't have access to the file");
+                    }
+                }
+           }         
         }
     }
 
@@ -66,19 +57,6 @@ public class Client
         return exist; 
     }
 
-    // public static void fileExists() {
-    //     String project = System.getProperty("user.dir");
-    //     File directory = new File(project);
-    //     File[] files = directory.listFiles();
-
-    //     for (File search : files) {
-    //         String f = search.getName();
-    //         System.out.println(f);
-    //         System.out.println("Found the file: " + search.getAbsolutePath());
-    //     }
-    // }
-
-
     public static boolean verifyNumber(String input){
         boolean response = true;
         int inputNumber = 0;
@@ -87,14 +65,33 @@ public class Client
         }else{
             try{
                 inputNumber = Integer.parseInt(input);
+                if(inputNumber < 1){
+                    response = false;
+                }
             } catch (NumberFormatException nfe) {
                 response = false;
             }
         }
-        if(inputNumber < 1){
-            response = false;
-        }
         return response;
     }
+
+    public static String generateRandomGUII(){
+        UUID uuid = UUID.randomUUID();
+        String uuidString = uuid.toString();
+        return uuidString;
+    }
+
+    public static String readUUID (String file) {
+		BufferedReader reader;
+        String line = "";
+
+		try {
+			reader = new BufferedReader(new FileReader(file));
+			line = reader.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        return line;
+	}
 
 }
